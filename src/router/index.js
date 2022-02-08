cbc65a0aaa59ee7ac89791dc5ca11544defad419
Mahfoud Bousba <mahfoudbousba2@gmail.com>
2022-02-08 18:49:40 -0500

    Add a redirect file

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
 import DevSkills from '../views/DevSkills.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
