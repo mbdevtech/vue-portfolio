0f824e70509170254786433fba1f250853842d29
Mahfoud Bousba <mahfoudbousba2@gmail.com>
2022-02-07 22:40:56 -0500

    Fix some css issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
-import Transition from '../views/Transition.vue'
 import DevSkills from '../views/DevSkills.vue'
 import Work from '../views/Work.vue'
 
@@ -22,11 +21,6 @@ const routes = [
     component: Contact
   },
   {
-    path: '/transition',
-    name: 'Transition',
-    component: Transition
-  },
-   {
     path: '/work',
     name: 'work',
     component: Work
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,10 +5,10 @@
     <hr><br><br>
     <div class="container">
         <div class="row" style="margin:20px;">
-        <div class="col-md-6">
+        <div class="col-md-7">
             <img src="@/assets/contact.svg" class = "img" alt="">
         </div>
-        <div class="col-md-6">
+        <div class="col-md-5">
           <div class="info">
             <img src="@/assets/mail.png" alt="" class="thumb">
             <span><h5>mahfoud_bousba@yahoo.com</h5></span>
@@ -20,11 +20,11 @@
           <div class="links">
             <div class="info">
                <img src="@/assets/linkedin.png" alt="" class="thumb">
-               <span>https://www.linkedin.com/in/mahfoud-bousba-30ba801b</span>
+               <span><h5>linkedin.com/in/mahfoud-bousba-30ba801b</h5></span>
             </div>
             <div class="info">
                <img src="@/assets/github.png" alt="" class="thumb">
-               <span>https://github.com/mbdevtech</span>
+               <span><h5>https://github.com/mbdevtech</h5></span>
             </div>          
           </div>
         </div>
@@ -49,11 +49,13 @@ export default {
   width: 70%;
 }
 .thumb{
-  width: 8%;
+  width: 10%;
   margin-right: 10px;
+  margin-bottom: 5px;
 }
 .info{
-  margin-top: 20px;
-  display: flex;
+  margin-top: 15px;
+  margin-bottom: 8px;
+  display:block;
 }
 </style>
